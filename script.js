3835b1ad4de12182d66853e85e0d19a75278c1cd
Helena Johansson <helena@klarr.se>
2021-12-05 12:17:07 +0100

    starter code uploaded

---
+++ script.js
@@ -0,0 +1,32 @@
+// Select DOM elements
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+// initialize array
+let data = [];
+
+// get random user
+
+// double everyones money
+
+// sort by richest person
+
+// show ony millionaires
+
+// calculate total wealth
+
+// add new user to data array
+
+// update the DOM
+
+// format money
+// https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// event listeners
